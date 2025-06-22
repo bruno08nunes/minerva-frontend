2d78289e8d075a21b3a4b34e5da562a059eeac53
bruno08nunes <brunonunes.sl08@gmail.com>
2025-06-21 21:34:00 -0300

    feat: enhance Exercise component with navigation and input handling improvements

--- src/components/exercise/Exercise.tsx
+++ src/components/exercise/Exercise.tsx
@@ -4,26 +4,46 @@ import { Lesson } from "@/types/lesson";
 import { useState } from "react";
 import ExerciseParagraph from "./ExerciseParagraph";
 import ExerciseCode from "./ExerciseCode";
+import { useRouter } from "next/navigation";
 
 export default function ExerciseComponent({ lesson }: { lesson: Lesson }) {
     const [inputValue, setInputValue] = useState("");
     const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
-    const exercise: Exercise = lesson.exercises[currentExerciseIndex];
+    const exercise: Exercise =
+        lesson.exercises[currentExerciseIndex % lesson.exercises.length];
+
+    const router = useRouter();
+
+    function updateExercise() {
+        if (currentExerciseIndex + 1 >= lesson.exercises.length) {
+            router.push(`/learn/lessons/${lesson.theme.slug}/${lesson.topic.slug}`);
+        }
+        setCurrentExerciseIndex((state) => state + 1);
+    }
 
     function handleSubmitChoice(isCorrect: boolean) {
         alert(isCorrect ? "Correto" : "Errado");
+        if (isCorrect) {
+            updateExercise();
+        }
     }
-
+    
     function handleChangeInput(value: string) {
         setInputValue(value);
     }
-
+    
     function handleSubmitInputAnswer() {
-        alert(inputValue === exercise.choices[0].text ? "Correto" : "Errado");
+        const isCorrect = inputValue === exercise.choices[0].text;
+        alert(isCorrect ? "Correto" : "Errado");
+        setInputValue("");
+        if (isCorrect) {
+            updateExercise();
+        }
     }
 
     return (
-        <div>
+        <section>
+            {/* Exercise Content */}
             {exercise.content.map((item, index) => (
                 <div key={index}>
                     {item.type === "paragraph" ? (
@@ -37,6 +57,8 @@ export default function ExerciseComponent({ lesson }: { lesson: Lesson }) {
                     )}
                 </div>
             ))}
+
+            {/* Submit Button for Write Code/Complete Code Exercises */}
             {["WRITE_CODE", "COMPLETE_CODE"].includes(exercise.type) && (
                 <button
                     onClick={handleSubmitInputAnswer}
@@ -45,8 +67,10 @@ export default function ExerciseComponent({ lesson }: { lesson: Lesson }) {
                     Enviar
                 </button>
             )}
+
+            {/* Choices for Multiple Choice Exercises */}
             {exercise.type === "MULTIPLE_CHOICE" && (
-                <section className="grid grid-cols-2 items-center justify-items-center max-w-[800px] mx-auto gap-4 max-[500px]:grid-cols-1">
+                <div className="grid grid-cols-2 items-center justify-items-center max-w-[800px] mx-auto gap-4 max-[500px]:grid-cols-1">
                     {exercise.choices.map((item) => (
                         <button
                             key={item.id}
@@ -56,21 +80,8 @@ export default function ExerciseComponent({ lesson }: { lesson: Lesson }) {
                             {item.text}
                         </button>
                     ))}
-                </section>
+                </div>
             )}
-            <button
-                className="bg-plum text-lavender-blush absolute bottom-[30%] left-[50%] translate-[0 -50%] p-2 rounded-2xl cursor-pointer"
-                onClick={() =>
-                    setCurrentExerciseIndex((state) => {
-                        if (state + 1 === lesson.exercises.length) {
-                            return 0;
-                        }
-                        return state + 1;
-                    })
-                }
-            >
-                Teste
-            </button>
-        </div>
+        </section>
     );
 }
