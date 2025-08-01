76078eab21ed8b3a71c301fcde30136c9bbfc7c0
bruno08nunes <brunonunes.sl08@gmail.com>
2025-07-31 21:13:53 -0300

    style: enhance ranking page layout and typography for better readability

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -9,21 +9,21 @@ export default async function RankingPage() {
         <>
             <Header />
             <H1 title="Ranking" />
-            <section className="max-w-[800px] mx-auto w-full">
+            <section className="max-w-[800px] mx-auto w-full p-3">
                 <ul className="flex flex-col gap-4">
                     {success ? (
                         users.map((user, index) => (
                             <li
                                 key={index}
-                                className="bg-plum text-lavender-blush text-2xl p-4 rounded-xl flex justify-between items-center"
+                                className="bg-plum text-lavender-blush sm:text-2xl text-md p-4 rounded-xl flex justify-between items-center"
                             >
                                 <div className="flex gap-4 items-center">
-                                    <span className="font-bold text-3xl">
+                                    <span className="font-bold sm:text-3xl text-xl">
                                         {index + 1}°
                                     </span>
                                     <span>@{user.username}</span>
                                 </div>
-                                <span>{user.semanalXP}XP Semanal</span>
+                                <span>{user.semanalXP}XP</span>
                             </li>
                         ))
                     ) : (
