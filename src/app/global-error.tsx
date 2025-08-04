81748552f9d1f336eb6d20f92cfbcb9bc02b147e
bruno08nunes <brunonunes.sl08@gmail.com>
2025-08-04 08:48:23 -0300

    feat: update GlobalError component to improve error display and remove reset functionality

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,30 +1,33 @@
 "use client";
 import H1 from "@/components/layout/H1";
+import Image from "next/image";
+import image from "../../public/programming2.png";
+import Link from "next/link";
 
 export default function GlobalError({
     error,
-    reset,
 }: {
     error: Error & { digest?: string };
-    reset: () => void;
 }) {
     return (
         <html>
             <head>
                 <title>Erro - Minerva</title>
             </head>
-            <body className="text-center text-lavender-blush text-xl">
+            <body className="text-center text-lavender-blush text-xl p-3">
                 <H1 title="Erro!" />
+                <Image
+                    src={image}
+                    alt=""
+                    className="block mx-auto max-w-[250px]"
+                />
                 <h2 className="text-2xl mb-2">
                     Estamos passando por problemas! Tente novamente mais tarde.
                 </h2>
-                <p>{error.message}</p>
-                <button
-                    className="block w-max mx-auto my-4 border-current border py-2 px-3 hover:scale-105 transition-transform cursor-pointer"
-                    onClick={() => reset()}
-                >
-                    Tente novamente!
-                </button>
+                <p className="mb-2">{error.message}</p>
+                <Link href={"/"} className="underline">
+                    Volte para a página principal
+                </Link>
             </body>
         </html>
     );
