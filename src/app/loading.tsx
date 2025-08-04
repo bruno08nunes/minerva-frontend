6e65516079f39458b1b5d47c91cdc9a0b144dc11
bruno08nunes <brunonunes.sl08@gmail.com>
2025-08-04 09:07:11 -0300

    feat: add LoadingPage component with animated loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import { LoaderCircle } from "lucide-react";
+
+export default function LoadingPage() {
+    return <LoaderCircle color="#D3B1C2" size={100} className="animate-spin fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />;
+}
