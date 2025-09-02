571a93b8a8fcb3ea7f545a8d14c71375ad7b64a5
BRUNO NUNES <05563115070@senacrs.edu.br>
2025-09-02 15:43:10 -0300

    feat: create createExplanationAction

---
+++ src/action/admin/edit/edit-explanation-action.ts
@@ -0,0 +1,91 @@
+"use server";
+
+import { env } from "@/lib/env";
+import { parseMarkdownLike } from "@/utils/parseMarkdown";
+import { revalidateTag } from "next/cache";
+import z from "zod";
+
+export const editExplanationAction = async (
+    _prevState: { success: boolean; message: string },
+    formData: FormData
+) => {
+    const title = formData.get("title");
+    const description = formData.get("description");
+    const contentText = formData.get("content")?.toString();
+    const content = parseMarkdownLike(contentText!);
+    const topicId = formData.get("topicId");
+    const token = formData.get("token");
+    const id = formData.get("id");
+
+    const authenticateBodySchema = z.object({
+        title: z.string().optional(),
+        description: z.string().optional(),
+        content: z
+            .array(
+                z.object({
+                    type: z.enum(["paragraph", "code"]),
+                    data: z.string(),
+                })
+            )
+            .optional(),
+        topicId: z.string().uuid().optional(),
+    });
+
+    const { success } = authenticateBodySchema.safeParse({
+        title,
+        description,
+        content,
+        topicId
+    });
+
+    if (!success) {
+        return { success: false, message: "Dados inválidos!" };
+    }
+
+    try {
+        const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/explanations/${id}`, {
+            method: "PUT",
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+            body: formData,
+        });
+        const result = await res.json();
+        revalidateTag("explanations");
+
+        if (res.status === 404) {
+            return {
+                success: false,
+                message: "Explicação não encontrada!",
+            };
+        }
+
+        if (res.status === 400) {
+            return {
+                success: false,
+                message: "Informações incorretas",
+            };
+        }
+
+        if (!result.success && !res.ok) {
+            return {
+                success: false,
+                message: "Erro ao editar explicação. Tente novamente mais tarde.",
+            };
+        }
+
+        return {
+            success: true,
+            message: "Sucesso!",
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            success: false,
+            message:
+                err instanceof Error
+                    ? err.message
+                    : "Erro ao editar explicação. Tente novamente mais tarde.",
+        };
+    }
+};
