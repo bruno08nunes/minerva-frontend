a30d8c4d39bbeeaa27bd429d080f5d3a3d947f36
BRUNO NUNES <05563115070@senacrs.edu.br>
2025-09-16 14:25:33 -0300

    feat: add exercises pages

--- src/app/admin/exercises/[lessonId]/page.tsx
+++ src/app/admin/exercises/[lessonId]/page.tsx
@@ -8,22 +8,28 @@ export default async function ExerciseAdminPage({
     params: Promise<{ lessonId: string }>;
 }) {
     const { lessonId } = await params;
-    const {lessonData: lesson} = await getLessonById(lessonId);
+    const { lessonData: lesson } = await getLessonById(lessonId);
 
     if (!lesson) {
         redirect("/admin");
     }
 
-    console.log(lesson)
+    console.log(lesson);
 
     return (
         <section className="w-full">
-            <H1 title={`Editar Exercícios de ${lesson.name}`} />
-            {
-                lesson.exercises.map((exercise) => (
-                    <p key={exercise.order}>{exercise.order}</p>
-                ))
-            }
+            <H1 title={`Editar Exercícios de "${lesson.name}"`} />
+            <div className="flex flex-col gap-3">
+                {lesson.exercises.map((exercise) => (
+                    <div
+                        className="text-lavender-blush bg-plum p-4 rounded-md flex gap-3 items-center text-xl"
+                        key={exercise.order}
+                    >
+                        <span>{exercise.order}°</span>
+                        <p className="line-clamp-2">{exercise.content[0].data}</p>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 }
