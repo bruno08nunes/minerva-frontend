c915dd93dff290a4e4e29d93aeaeb63d800cfc9b
bruno08nunes <brunonunes.sl08@gmail.com>
2026-03-25 16:48:27 -0300

    feat: add lesson progression check and redirect logic to LessonExercisePage

--- src/app/learn/lesson/[lessonId]/page.tsx
+++ src/app/learn/lesson/[lessonId]/page.tsx
@@ -1,9 +1,11 @@
 import ExerciseComponent from "@/components/exercise/Exercise";
 import H1 from "@/components/layout/H1";
 import Header from "@/components/layout/Header";
-import { getLessonById } from "@/lib/api/lessons";
+import { getLessonById, listLessonsByTopicAndTheme } from "@/lib/api/lessons";
+import { Lesson } from "@/types/lesson";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 interface LessonExercisePageProps {
     params: Promise<{ lessonId: string }>;
@@ -46,12 +48,55 @@ export async function generateMetadata({
     };
 }
 
+const checkCanMakeLesson = (lessonsArray: Lesson[], currentLesson: Lesson) => {
+    const redirectUrl =
+        "/learn/lessons/" +
+        currentLesson?.theme.slug +
+        "/" +
+        currentLesson?.topic.slug;
+
+    const isFirstLesson = currentLesson.order <= 1;
+    if (!isFirstLesson) {
+        const lastLessonProgress = lessonsArray.find(
+            (lesson) => lesson.order === currentLesson.order - 1,
+        )?.Progress;
+
+        const hasProgress = lastLessonProgress && lastLessonProgress[0];
+        if (!hasProgress || !lastLessonProgress[0].isCompleted) {
+            redirect(redirectUrl);
+        }
+    }
+
+    const currentLessonProgress = lessonsArray.find(
+        (lesson) => lesson.order === currentLesson!.order,
+    )?.Progress;
+
+    const hasProgress = currentLessonProgress && currentLessonProgress[0];
+    if (hasProgress) {
+        if (currentLessonProgress[0].isCompleted) {
+            redirect(redirectUrl);
+        }
+    }
+};
+
 export default async function LessonExercisePage({
     params,
 }: LessonExercisePageProps) {
     const { lessonId } = await params;
     const { success, lessonData, message } = await getLessonById(lessonId);
     const token = (await cookies()).get("token");
+
+    if (success) {
+        const { lessonsData: lessonsArray } = await listLessonsByTopicAndTheme({
+            themeSlug: lessonData!.theme.slug,
+            topicSlug: lessonData!.topic.slug,
+            token: token?.value,
+        });
+
+        if (lessonsArray) {
+            checkCanMakeLesson(lessonsArray!, lessonData!);
+        }
+    }
 
     return (
         <>
